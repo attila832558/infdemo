37f1575d4f462b5b323181c02b720de7862e67d8
attila832558 <ubikutya2003@gmail.com>
2022-12-12 15:15:22 +0100

    Soviet edition

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        /*info about the program*/
-        System.out.println("This program will calculate the area of a circle.");
-        /*exit the program*/
-        System.exit(0);
-    }
-}
---
+++ src/main.java
@@ -0,0 +1,22 @@
+
+
+
+public class main {
+    /*create hello in soviet*/
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+        /*calculate the valadimir putin's age*/
+        int year = 1952;
+        int age = 2020 - year;
+        System.out.println("Владимир Владимирович Путин исполнилось " + age + " лет");
+
+
+
+
+
+
+
+    }
+}
+
+

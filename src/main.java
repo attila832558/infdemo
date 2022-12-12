cc7d477d3101ed1ab4db319122f3f349d43c4f48
jegyvasarlas <88721560+jegyvasarlas@users.noreply.github.com>
2022-12-12 15:24:37 +0100

    alma

--- src/main.java
+++ src/main.java
@@ -1,21 +1,26 @@
-
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class main {
-    /*create hello in soviet*/
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
-        /*calculate the valadimir putin's age*/
-        int year = 1952;
-        int age = 2020 - year;
-        System.out.println("Владимир Владимирович Путин исполнилось " + age + " лет");
-
-
-
-
-
-
-
+        System.out.println("Hello World!");
+        // Make a program that generates the lottery numbers
+        // 5 numbers between 1 and 90
+        // 1. make a list of numbers
+        // 2. shuffle the list
+        // 3. pick the first 5 numbers
+        // 4. sort the numbers
+        // 5. print the numbers
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 90; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        List<Integer> winningNumbers = numbers.subList(0, 5);
+        Collections.sort(winningNumbers);
+        System.out.println(winningNumbers);
+        // Powered by AI
     }
 }
 
